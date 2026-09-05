8386e13a6598c06e1d7a2125e7b24fe82d79dd02
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-09-05 05:05:16 +0300

    Release v0.2.85

--- examples/run.py
+++ examples/run.py
@@ -135,7 +135,7 @@ LANGUAGES = (
         BENCHMARK_DIR / "compose.go-native.yml",
         verify_framework_pool=False,
         repository="https://github.com/gorundebug/gonativeexample.git",
-        revision="v0.2.84",
+        revision="v0.2.85",
     ),
     Language("cpp", ROOT / "cppexample", BENCHMARK_DIR / "compose.cpp.yml"),
     Language(
@@ -144,7 +144,7 @@ LANGUAGES = (
         BENCHMARK_DIR / "compose.cpp-native.yml",
         verify_framework_pool=False,
         repository="https://github.com/gorundebug/cppnativeexample.git",
-        revision="v0.2.84",
+        revision="v0.2.85",
     ),
     Language(
         "cpp-boost",
@@ -157,7 +157,7 @@ LANGUAGES = (
         BENCHMARK_DIR / "compose.cpp-boost-native.yml",
         verify_framework_pool=False,
         repository="https://github.com/gorundebug/cppboostnativeexample.git",
-        revision="v0.2.84",
+        revision="v0.2.85",
     ),
     Language("python", ROOT / "pyexample", BENCHMARK_DIR / "compose.python.yml"),
     Language(
@@ -166,7 +166,7 @@ LANGUAGES = (
         BENCHMARK_DIR / "compose.python-native.yml",
         verify_framework_pool=False,
         repository="https://github.com/gorundebug/pynativeexample.git",
-        revision="v0.2.84",
+        revision="v0.2.85",
     ),
     Language("rust", ROOT / "rustexample", BENCHMARK_DIR / "compose.rust.yml"),
     Language(
@@ -175,7 +175,7 @@ LANGUAGES = (
         BENCHMARK_DIR / "compose.rust-native.yml",
         verify_framework_pool=False,
         repository="https://github.com/gorundebug/rustnativeexample.git",
-        revision="v0.2.84",
+        revision="v0.2.85",
     ),
     Language(
         "typescript",
@@ -190,7 +190,7 @@ LANGUAGES = (
         repository="https://github.com/gorundebug/tsnativeexample.git",
         # Managed by servicegen's atomic release script. ``main`` is permitted
         # only until the first TypeScript-native release tag is published.
-        revision="v0.2.84",
+        revision="v0.2.85",
     ),
 )
 
